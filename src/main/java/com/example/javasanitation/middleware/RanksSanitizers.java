780f6b734378c21857d7088cd4327b486fb51331
mattrank <matthewrank93@gmail.com>
2021-11-03 14:27:58 -0500

    Added second method that returns boolean of good vs bad input.

--- src/main/java/com/example/javasanitation/middleware/RanksSanitizers.java
+++ src/main/java/com/example/javasanitation/middleware/RanksSanitizers.java
@@ -11,9 +11,10 @@ public class RanksSanitizers {
     /**
      * This method will strip away characters and character strings that are known to be used
      * for Nosql injection against MongoDb. The method is written so that it will not remove characters from
-     * standard user input.
-     * @param unsanitized
-     * @return
+     * standard user input. The method may not completely eliminate bad user input but will sufficiently disrupt the
+     * command rendering it ineffective.
+     * @param unsanitized the original input string
+     * @return returns the string after bad characters/phrases removed.
      */
     public String MongoInput(String unsanitized){
         Pattern pattern = Pattern.compile(
@@ -24,5 +25,20 @@ public class RanksSanitizers {
         return pattern.matcher(unsanitized).replaceAll("");
     }
 
+
+    /**
+     * Uses the same logic as the previous method but returns a boolean value if a simple check is needed
+     * @param unsanitized the original input string
+     * @return returns the string after bad characters/phrases removed.
+     */
+    public boolean MongoInputCheck(String unsanitized){
+        Pattern pattern = Pattern.compile(
+                "(gte)|(gt)|(eq)|(lt)|(lte)|[$]|" +
+                        "(sleep)|[()]|[;]|[:]|[ ]|(ne)|" +
+                        "(where:)|('1 == 1')|[{}]|(password)"
+                , Pattern.CASE_INSENSITIVE);
+        String sanitized = pattern.matcher(unsanitized).replaceAll("");
+        return (!(unsanitized.equals(sanitized)));
+    }
 
 }
